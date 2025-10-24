a315c03b13c85524ae511e73fbe1cd95bb740bdc
burcaaurelia-art <burcaaurelia@gmail.com>
2025-10-24 13:16:47 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-black/90 backdrop-blur-md border-b-4 border-red-500 shadow-lg">
+    <nav className="fixed top-0 left-0 w-full z-50 bg-black/90 backdrop-blur-md border-b-4 border-red-500 shadow-lg mt-10">
       <div className="max-w-7xl mx-auto flex items-center justify-between py-5 px-6 sm:py-6 mt-3 sm:mt-4">
         {/* LOGO */}
         <Link
